21968a294cf1f078e147745d1c2a21d7e23c345b
XiaoGuo, Liu <liu_xiao_guo@yahoo.com>
2016-10-28 12:25:26 +0800

    Initial release

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+
+from setuptools import setup, find_packages
+
+setup(
+  name='livevideo',
+  version='0.1',
+  description='livevideo',
+  long_description=('livevideo'),
+  url='http://github.com/liu-xiao-guo/livevideo',
+  license='MIT',
+  author='xiaoguo, liu',
+  author_email='liu-xiao-guo@yahoo.com',
+  packages=find_packages(exclude=['tests*']),
+  include_package_data=True,
+  entry_points = {
+  'console_scripts': [
+    'create_db = video.stream:main'
+  ]
+  },
+  package_data={
+    'static': 'video/static/*',
+    'templates': 'video/templates/*'},
+  classifiers=[
+    "Private :: Do Not Upload"
+  ],
+)
---
+++ video/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import  absolute_import
+from . import camera
+from . import stream
+__all__ = ["camera", "stream"]
